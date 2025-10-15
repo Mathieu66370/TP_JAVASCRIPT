5bb72b0be7e4334ec0a63d3c423af670a4bdcaaa
Mathieu <bosq.mathieu@gmail.com>
2025-10-15 17:21:57 +0200

    maj des derniers fichiers

---
+++ CalculMoyenne.js
@@ -0,0 +1,10 @@
+let array = [1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4];
+
+// Calcul de la somme
+let sum = array.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+// Calcul de la moyenne
+let average = sum / array.length;
+
+// Affichage
+console.log("La moyenne des éléments du tableau est :", average);
---
+++ FirstLast.js
@@ -0,0 +1,5 @@
+let array = [5, 2, 3, 5]; // Exemple de tableau
+
+let check = array.length >= 1 && array[0] === array[array.length - 1];
+
+console.log(check);
