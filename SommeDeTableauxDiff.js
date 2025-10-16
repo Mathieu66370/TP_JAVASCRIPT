7489ff98af463a2b5e05323e9a43b4acc2309014
Mathieu <bosq.mathieu@gmail.com>
2025-10-16 09:58:19 +0200

    MAJ2

--- SommeDeTableauxDiff.js
+++ SommeDeTableauxDiff.js
@@ -1,8 +1,12 @@
 let array1 = [1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4];
 let array2 = [-1, 12, 17, 14, 5, -9, 0, 18];
+
+// Déterminer la longueur maximale
+let maxLength = Math.max(array1.length, array2.length);
+
 // Créer le tableau somme
 let sumArray = [];
-for (let i = 0; i < Length; i++) {
+for (let i = 0; i < maxLength; i++) {
     let val1 = array1[i] !== undefined ? array1[i] : 0;
     let val2 = array2[i] !== undefined ? array2[i] : 0;
     sumArray.push(val1 + val2);
